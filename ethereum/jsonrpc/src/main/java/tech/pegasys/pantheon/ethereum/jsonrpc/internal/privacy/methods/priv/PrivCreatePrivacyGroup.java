073231c0d08addaa164cbbf23b079cd8c8e598ad
Ivaylo Kirilov <iikirilov@gmail.com>
2019-08-28 16:45:07 +0100

    add error logging to jsonrpc handler

--- ethereum/jsonrpc/src/main/java/tech/pegasys/pantheon/ethereum/jsonrpc/internal/privacy/methods/priv/PrivCreatePrivacyGroup.java
+++ ethereum/jsonrpc/src/main/java/tech/pegasys/pantheon/ethereum/jsonrpc/internal/privacy/methods/priv/PrivCreatePrivacyGroup.java
@@ -73,6 +73,7 @@ public class PrivCreatePrivacyGroup implements JsonRpcMethod {
     try {
       response = enclave.createPrivacyGroup(createPrivacyGroupRequest);
     } catch (Exception e) {
+      LOG.error("Failed to create privacy group", e);
       return new JsonRpcErrorResponse(request.getId(), JsonRpcError.CREATE_PRIVACY_GROUP_ERROR);
     }
     return new JsonRpcSuccessResponse(request.getId(), response.getPrivacyGroupId());
